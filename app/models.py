33093a8a179fb189340ecc6d3f64a6b61a03c7a6
krnblni <karanbalani007@gmail.com>
2020-02-16 15:10:28 +0530

    added followers support

--- app/models.py
+++ app/models.py
@@ -4,18 +4,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+
 @login.user_loader
 def loadUser(id):
     return User.query.get(int(id))
 
+
+followers = db.Table(
+    "followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+)
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(128), index = True, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship("Post", backref = "author", lazy = "dynamic")
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(128))
-    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        "User", secondary = followers, 
+        primaryjoin=(followers.c.follower_id == id), 
+        secondaryjoin=(followers.c.followed_id == id), 
+        backref = db.backref("followers", lazy="dynamic"), 
+        lazy="dynamic"
+    )
 
     def __repr__(self):
         return "<User: {}>".format(self.username)
@@ -30,10 +45,27 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    def follow(self, user):
+        if not self.isFollowing(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.isFollowing(user):
+            self.followed.remove(user)
+
+    def isFollowing(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followedPosts(self):
+        othersPosts = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        ownPosts = Post.query.filter_by(user_id = self.id)
+        return othersPosts.union(ownPosts).order_by(Post.timestamp.desc())
+
+
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -78,3 +78,33 @@ def editProfile():
         form.username.data = current_user.username
         form.aboutMe.data = current_user.about_me
     return render_template("edit_profile.html", title = "Edit Profile", form = form)
+
+@app.route("/follow/<username>")
+@login_required
+def follow(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        flash("No user found with username '{}'".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("You cannot follow yourself!")
+        return redirect(url_for("user", username = username))
+    current_user.follow(user)
+    db.session.commit()
+    flash("Great! You are now following '{}'".format(username))
+    return redirect(url_for("user", username = username))
+
+@app.route("/unfollow/<username>")
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        flash("No user found with username '{}'".format(username))
+        return redirect(url_for("index"))
+    if user == current_user:
+        flash("You cannot unfollow yourself!")
+        return redirect(url_for("user", username = username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash("That's sad! You unfollowed '{}'".format(username))
+    return redirect(url_for("user", username = username))
